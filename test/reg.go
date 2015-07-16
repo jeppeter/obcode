0635cfab5c7f2d61451458090db928bc9ca06c67
jeppeter <jeppeter@gmail.com>
2015-07-16 14:28:16 +0800

    add regular expression test into repo

---
+++ test/reg.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Fprintf(os.Stderr, "%s exp string", os.Args[0])
+		os.Exit(1)
+	}
+	re, e := regexp.Compile(os.Args[1])
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "compile %s error %v\n", os.Args[1], e)
+		os.Exit(2)
+	}
+
+	buf := []byte(os.Args[2])
+	b := re.Match(buf)
+
+	if b {
+		fmt.Fprintf(os.Stdout, "(%s) match (%s)\n", os.Args[1], os.Args[2])
+	} else {
+		fmt.Fprintf(os.Stdout, "(%s) not match (%s)\n", os.Args[1], os.Args[2])
+	}
+	return
+}
